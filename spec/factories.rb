e940d21ce18f4c813c9e03ddba66da0ffd762ed7
PenneyGadget <dreamofgreen@gmail.com>
2016-05-23 10:51:08 -0600

    Adds links model

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,10 @@
 FactoryGirl.define do
+  factory :link do
+    url "MyString"
+    title "MyString"
+    read_status false
+    references ""
+  end
   factory :user do
     email "penney@email.com"
     password "password"
